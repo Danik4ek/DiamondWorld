5c61981c9a0c506821e43ffcd7d31d231fbb731e
Danik4ek <137286837+Danik4ek@users.noreply.github.com>
2025-05-18 15:08:55 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,9 +5,3 @@ buttonId.addEventListener("click",function(){
     counter++;
     balId.textContent=counter
 });
-const AdController =  window.AdsModulePgram;
-AdController.show({ AppId: "6399538" }).then((result) => {
-    console.log(result); // Logged result in the console
-}).catch((error) => {
-    console.log(error); // Logged error in the console
-});
